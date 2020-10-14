148e8788925240916e5e8b3441b6cdba6cd48775
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-10-14 14:21:15 +0500

    App Updated

--- src/Context/GlobalContext.js
+++ src/Context/GlobalContext.js
@@ -1,8 +1,31 @@
-import React, { createContext, useReducer } from 'react'
-import cartReducer from './cartReducer'
+import React, { createContext, useReducer } from "react";
+import cartReducer from "./cartReducer";
 
 export const GlobalContext = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(cartReducer, []);
-}
+  const [state, dispatch] = useReducer(cartReducer, []);
+
+  const addItem = (payload) =>
+    dispatch({
+      type: "ADD",
+      payload,
+    });
+
+  const removeItem = (payload) =>
+    dispatch({
+      type: "REMOVE",
+      payload,
+    });
+
+  const payment = () =>
+    dispatch({
+      type: "PAY",
+    });
+
+  return (
+    <GlobalContext.Provider value={{ state, addItem, removeItem, payment }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
