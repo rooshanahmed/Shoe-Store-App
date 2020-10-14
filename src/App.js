c3ec953052feef4da490afb386094868d9c49701
Rooshan <rooshan.ahmed@hotmail.com>
2020-10-15 01:40:07 +0500

    App Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Components/Header';
 
 
 function App() {
   return (
-    <div className="App">
-      Store App
+    <div>
+      <Header />
     </div>
   );
 }
