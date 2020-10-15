4803810f184507ca2674295268a0631d8ce5d901
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-10-16 02:49:31 +0500

    App Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from './Components/Header';
+import { CssBaseline } from '@material-ui/core';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
 
 
 function App() {
   return (
     <div>
+      <CssBaseline />
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
