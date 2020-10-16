054028a9655e4ef6bffb883a18ee47ec5cc81ec7
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-10-17 00:04:35 +0500

    App Updated

---
+++ src/data.js
@@ -0,0 +1,114 @@
+export default [
+  {
+    id: 1,
+    name: "lite racer adapt 3.0",
+    price: 59,
+    title: "a lightweight shoe with slip-on convenience.",
+    description:
+      "From heading to the gym early to hitting the streets for daily activities, these adidas shoes deliver. The slip-on construction gives them a snug and lightweight fit, and the midsole cushioning cradles your foot in cloud-like comfort.",
+    images: [
+      require("./resources/shoe-images/shoe1-color1.png"),
+      require("./resources/shoe-images/shoe1-color2.png"),
+      require("./resources/shoe-images/shoe1-color3.png"),
+      require("./resources/shoe-images/shoe1-color4.png"),
+    ],
+  },
+  {
+    id: 2,
+    name: "zx 2k boost",
+    price: 150,
+    title: "future-ready trainers with wrapped boost for exceptional comfort.",
+    description:
+      "ZX takes an icon to a new dimension of comfy. Innovated for the ultimate squish with new wrapped BOOST, ZX never felt so good. A springy Boost midsole swaddled in a wrap-around gradient. A progressive look and technical design, brimming with Boost. Feels too good to be explained.",
+    images: [
+      require("./resources/shoe-images/shoe2-color1.png"),
+      require("./resources/shoe-images/shoe2-color2.png"),
+      require("./resources/shoe-images/shoe2-color3.png"),
+      require("./resources/shoe-images/shoe2-color4.png"),
+    ],
+  },
+  {
+    id: 3,
+    name: "ultraboost 20",
+    price: 180,
+    title: "responsive running shoes with stitched-in support.",
+    description:
+      "Already a legend five years in, the adidas Ultraboost gets a colorful update. Pink equals powerful. Embrace the color that represents a drive for unity and meaningful change. The famously responsive cushioning has a bright hue that matches a stretchy knit upper that's made with recycled materials. Stitched-in reinforcement right where you need it keeps you supported and running at your best.",
+    images: [
+      require("./resources/shoe-images/shoe3-color1.png"),
+      require("./resources/shoe-images/shoe3-color2.png"),
+      require("./resources/shoe-images/shoe3-color3.png"),
+      require("./resources/shoe-images/shoe3-color4.png"),
+    ],
+  },
+  {
+    id: 4,
+    name: "marvel spidey sense",
+    price: 100,
+    title: "donovan mitchell issue #2, marvel spidey sense edition.",
+    description:
+      "Taking its cues from Donovan Mitchell's superhuman instincts, the D.O.N. Issue #2 Marvel Spidey Sense is a sight to be seen. These signature shoes from adidas Basketball have neon red pops along the lime green upper. A translucent glow-in-the-dark outsole presents a 'radioactive' silhouette that almost looks like it's blinking when it senses an imminent threat on the court. His Spidey sense must be tingling.",
+    images: [
+      require("./resources/shoe-images/shoe4-color1.png"),
+      require("./resources/shoe-images/shoe4-color2.png"),
+      require("./resources/shoe-images/shoe4-color3.png"),
+      require("./resources/shoe-images/shoe4-color4.png"),
+    ],
+  },
+  {
+    id: 5,
+    name: "stan smith",
+    price: 64,
+    title: "a tennis look from the '70s steps into the present.",
+    description:
+      "Fads may come and go but classics never lose their appeal. The adidas Stan Smith has been turning heads since 1971, and it doesn't have plans to slow down any time soon. Carry on the tradition of eye-catching style when you rock the clean leather look of a time-honored tennis staple. Unmistakably archival, yet always fresh, this version shows off 'Stan' stamped on one shoe and 'Smith' on the other.",
+    images: [
+      require("./resources/shoe-images/shoe5-color1.png"),
+      require("./resources/shoe-images/shoe5-color2.png"),
+      require("./resources/shoe-images/shoe5-color3.png"),
+      require("./resources/shoe-images/shoe5-color4.png"),
+    ],
+  },
+  {
+    id: 6,
+    name: "x9000l3",
+    price: 120,
+    title: "high-tech shoes inspired by gaming culture.",
+    description:
+      "Are you a gamer or are you an athlete? The answer is yes. adidas blended aspects of the virtual and the actual worlds to create these X9000L3 Shoes. Responsive cushioning in the forefoot and lightweight cushioning in the heel combine to keep your stride smooth and comfortable. The outsole tread pattern was inspired by computer-generated data and provides reliable traction on city streets.",
+    images: [
+      require("./resources/shoe-images/shoe6-color1.png"),
+      require("./resources/shoe-images/shoe6-color2.png"),
+      require("./resources/shoe-images/shoe6-color3.png"),
+      require("./resources/shoe-images/shoe6-color4.png"),
+    ],
+  },
+  {
+    id: 7,
+    name: "adidas 4d run 1.0",
+    price: 200,
+    title: "futuristic running shoes with a precisely tuned midsole.",
+    description:
+      "Be a part of the future of running. The distinctive lattice midsole on these adidas shoes was crafted from liquid resin to give you precisely tuned cushioning. Computer-generated data inspired the tread pattern on the rubber outsole.",
+    images: [
+      require("./resources/shoe-images/shoe7-color1.png"),
+      require("./resources/shoe-images/shoe7-color2.png"),
+      require("./resources/shoe-images/shoe7-color3.png"),
+      require("./resources/shoe-images/shoe7-color4.png"),
+    ],
+  },
+  {
+    id: 8,
+    name: "response super",
+    price: 81,
+    title: "supportive running shoes to get you to your goals.",
+    description:
+      "At the end of the day, what's 20 more seconds? Push yourself to go a little farther than yesterday in these adidas running shoes. Spring through every step with the energy-returning cushioning and let yourself fly. Lacing up and getting out was the right call.",
+    images: [
+      require("./resources/shoe-images/shoe8-color1.png"),
+      require("./resources/shoe-images/shoe8-color2.png"),
+      require("./resources/shoe-images/shoe8-color3.png"),
+      require("./resources/shoe-images/shoe8-color4.png"),
+    ],
+  },
+];
