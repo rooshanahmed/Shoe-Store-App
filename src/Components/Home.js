5c6cb74210cdc275521ce4609de76d3b5f4e180f
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-10-16 22:43:02 +0500

    App Updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,25 +1,59 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core'
-
+import React from "react";
+import { Button, makeStyles } from "@material-ui/core";
+import shoe from "../Images/shoe.png";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-    root:{
-        backgroundColor: "#1A2F43",
-        backgroundRepeat: "no-repeat",
-        display: "flex",
-        backgroundSize: "cover",
-        minHeight: "100vh",
-    }
-}))
+  root: {
+    backgroundColor: "#1A2F43",
+    backgroundRepeat: "no-repeat",
+    display: "flex",
+    backgroundSize: "cover",
+    minHeight: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontFamily: "Chilanka",
+    fontSize: "48px",
+    color: "#fff",
+  },
+  image: {
+    // marginLeft: "50px",
+    transform: "rotate(-35deg)",
+    Height: "70px",
+  },
+  button: {
+    marginTop: "2px",
+    fontSize: "16px",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+  },
+}));
 
 const Home = () => {
-    const classes = useStyles();
-    
-    return (
-        <div className={classes.root}>
-            
-        </div>
-    )
-}
+  const classes = useStyles();
+  const navigate = useNavigate();
 
-export default Home
+  return (
+    <div className={classes.root}>
+      <div>
+        <h2 className={classes.title}>Cercle Shoe Store</h2>
+        <Button
+          disableTouchRipple
+          variant="contained"
+          className={classes.button}
+          onClick={() => {
+            navigate("products");
+          }}
+        >
+          Shop Now!!
+        </Button>
+      </div>
+      <img src={shoe} alt="shoe" className={classes.image} />
+    </div>
+  );
+};
+
+export default Home;
