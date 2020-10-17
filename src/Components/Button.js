8d367ab5c4ff728b73a56b87084f415cd47163e2
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-10-17 11:47:02 +0500

    App Updated

---
+++ src/Components/Button.js
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import { makeStyles, Button, Tooltip } from "@material-ui/core";
+import { GlobalContext } from "../Context/GlobalContext";
+
+const useStyles = makeStyles((theme) => ({
+  btn:{
+    margin: "0 auto",
+    display: "block",
+  },
+}));
+
+const Button = ({ dataItem }) => {
+  const classes = useStyles();
+  const { state, addItem } = useContext(GlobalContext);
+  const isTrue = state.find((item) => item.id === dataItem.id);
+
+  return !isTrue ? (
+    <Button
+      variant="contained"
+      onClick={() => addItem(dataItem)}
+      className={classes.btn}
+    >
+      {" "}
+      Add To Cart{" "}
+    </Button>
+  ) : (
+    <Tooltip
+      title="Already In Cart"
+      placement="top-end"
+      enterDelay={400}
+      leaveDelay={200}
+      arrow
+    >
+      <Button
+        variant="contained"
+        onClick={() => addItem(dataItem)}
+        className={classes.btn}
+      >
+        {" "}
+        Add To Cart{" "}
+      </Button>{" "}
+    </Tooltip>
+  );
+};
+
+export default Button;
---
+++ src/Components/ProductDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import { useParams } from 'react-router-dom'
+import Button from './Button'
+
+const useStyles = makeStyles((theme) => ({}));
+
+const ProductDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductDetails
