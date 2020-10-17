5f9e13d5a0e7d00aaafb46f045bcfd0d1ec40b11
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-10-17 12:59:36 +0500

    App Updated

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -3,7 +3,68 @@ import { Grid, makeStyles, Paper, Typography } from "@material-ui/core";
 import { useParams } from "react-router-dom";
 import CartButton from "./Button";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: "0 auto",
+    // minWidth: "100%",
+    backgroundColor: "#1A2F43",
+    backgroundSize: "cover",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+  },
+  image: {
+    width: "100%",
+  },
+  heading: {
+    textTransform: "uppercase",
+    padding: theme.spacing(2),
+    paddingBottom: 0,
+    fontFamily: "Comfortaa",
+    color: "#fff",
+    fontSize: "26px",
+  },
+  price: {
+    padding: theme.spacing(2),
+    paddingTop: 0,
+    paddingBottom: 0,
+    fontWeight: 500,
+    fontFamily: "Comfortaa",
+    color: "#fff",
+    fontSize: "26px",
+  },
+  title: {
+    textTransform: "uppercase",
+    padding: theme.spacing(2),
+    paddingBottom: 0,
+    letterSpacing: "1px",
+    fontStyle: "italic",
+    fontFamily: "Comfortaa",
+    color: "#fff",
+    fontSize: "16px",
+  },
+  description: {
+    letterSpacing: "3px",
+    padding: theme.spacing(2),
+    fontFamily: "Comfortaa",
+    color: "#fff",
+    fontSize: "14px",
+  },
+  container: {
+    maxWidth: "80%",
+    margin: "0 auto",
+  },
+  avialableColor: {
+    height: 50,
+    width: 85,
+    border: "2px solid black",
+    cursor: "pointer",
+  },
+  grd: {
+    marginTop: 50,
+  },
+}));
 
 const ProductDetails = ({ data }) => {
   const [val, setVal] = useState(0);
@@ -13,8 +74,8 @@ const ProductDetails = ({ data }) => {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3} justify="center">
-        <Grid item xs={12} sm={12} ms={7}>
+      <Grid container spacing={0} justify="center" className={classes.grd}>
+        <Grid item xs={8} sm={5} md={6}>
           <Paper>
             <img
               src={dataItem.images[val]}
@@ -24,13 +85,16 @@ const ProductDetails = ({ data }) => {
           </Paper>
           <Grid
             container
-            spacing={3}
+            spacing={2}
             justify="center"
             className={classes.container}
             alignItems="center"
           >
             <Grid item xs={12} sm={12} md={4}>
-              <Typography variant="subtitle1" style={{ textAlign: "center" }}>
+              <Typography
+                variant="subtitle1"
+                style={{ textAlign: "center", color: "#fff" }}
+              >
                 4 Colors Avialable
               </Typography>
             </Grid>
