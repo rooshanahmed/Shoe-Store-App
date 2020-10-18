f00bde12feda9d016312d6152ab8004fc07cda7c
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-10-19 01:41:23 +0500

    Update ContactUs

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,61 +1,63 @@
-import React from 'react'
-import { makeStyles, Typography } from '@material-ui/core'
-import contact from '../Images/contact.png'
-import useWebAnimations, { fadeIn } from '@wellyshen/use-web-animations'
-
+import React from "react";
+import { makeStyles, Typography } from "@material-ui/core";
+import contact from "../Images/contact.png";
+import useWebAnimations, { fadeIn } from "@wellyshen/use-web-animations";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        // backgroundColor: "#1A2F43",
-        color: "#fff",
-        minHeight: "100vh",
-        flexDirection: "column",
-        backgroundColor: "black",
-    },
-    img: {
-        width: "750px",
-        height: "450px",
-    },
-    text: {
-        fontFamily: "Comfortaa",
-        fontSize: "28px",
-    },
-    email: {
-        fontFamily: "Comfortaa",
-        fontSize: "20px",
-        marginTop: "15px",
-    },
-    phone: {
-        fontFamily: "Comfortaa",
-        fontSize: "20px",
-    }
-}))
-
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: "#1A2F43",
+    color: "#fff",
+    minHeight: "100vh",
+    flexDirection: "column",
+    backgroundColor: "black",
+  },
+  img: {
+    width: "750px",
+    height: "450px",
+  },
+  text: {
+    fontFamily: "Comfortaa",
+    fontSize: "28px",
+  },
+  email: {
+    fontFamily: "Comfortaa",
+    fontSize: "20px",
+    marginTop: "15px",
+  },
+  phone: {
+    fontFamily: "Comfortaa",
+    fontSize: "20px",
+  },
+}));
 
 const ContactUs = () => {
-    const classes = useStyles();
-    const { keyframes: main , timing: mainTime } = fadeIn;
+  const classes = useStyles();
+  const { keyframes: main, timing: mainTime } = fadeIn;
 
-    const { ref } = useWebAnimations({
-        keyframes: main,
-        timing: {
-            ...mainTime,
-            delay: "1000",
-            easing: "ease-in",
-        }
-    })
+  const { ref } = useWebAnimations({
+    keyframes: main,
+    timing: {
+      ...mainTime,
+      delay: "1000",
+      easing: "ease-in",
+    },
+  });
 
-    return (
-        <div className={classes.root}>
-            <img src={contact} alt="contact" className={classes.img} ref={ref} />
-            <Typography className={classes.text}>You Can Contact Us Anytime</Typography>
-            <Typography className={classes.email}>Email: cerclestore@gmail.com</Typography>
-            <Typography className={classes.phone}>Phone: +1 123 456 7890</Typography>
-        </div>
-    )
-}
+  return (
+    <div className={classes.root}>
+      <img src={contact} alt="contact" className={classes.img} ref={ref} />
+      <Typography className={classes.text}>
+        You Can Contact Us Anytime
+      </Typography>
+      <Typography className={classes.email}>
+        Email: cerclestore@gmail.com
+      </Typography>
+      <Typography className={classes.phone}>Phone: +1 123 456 7890</Typography>
+    </div>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
