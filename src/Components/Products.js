1f533dbca42809f79ccffbc93aabf1e21e72acb1
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-10-19 01:44:21 +0500

    Update Products.js

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Products = () => {
-    return (
-        <div>
-           <Outlet /> 
-        </div>
-    )
-}
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
 
-export default Products
+export default Products;
