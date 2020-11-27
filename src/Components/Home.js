00bdfe8cd875b9fd30df7470cdb4c1ff787fb458
Rooshan <rooshan.ahmed@hotmail.com>
2020-11-27 19:17:44 +0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -28,12 +28,16 @@ const useStyles = makeStyles((theme) => ({
   button: {
     marginTop: "2px",
     fontSize: "16px",
+    width: "200px",
+    background: "transparent",
+    textTransform: "capitalize",
     justifyContent: "center",
     alignItems: "center",
     display: "flex",
     fontFamily: "Comfortaa",
+    border: "1px solid #7E83A0",
     "&:hover": {
-      backgroundColor: "black",
+      background: "linear-gradient(45deg, #671F02 30%, #972F06 90%)",
       color: "#fff",
     },
   },
@@ -50,7 +54,7 @@ const Home = () => {
           <h2 className={classes.title}>Cercle Shoe Store</h2>
           <Button
             disableTouchRipple
-            variant="contained"
+            disableElevation
             className={classes.button}
             onClick={() => {
               navigate("products");
